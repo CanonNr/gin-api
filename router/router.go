53e12492e38cbd4f4963c10137880249d576bac1
Canon <717437709@qq.com>
2020-08-04 11:58:04 +0800

    RabbitMq0.5 协程

--- router/router.go
+++ router/router.go
@@ -1,11 +1,13 @@
 package router
 
 import (
+	"fmt"
 	"gin-api/app/controller/rabbitmq"
 	"gin-api/app/controller/rabbitmq/timer"
 	"gin-api/app/controller/test"
 	"gin-api/router/middleware"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func SetupRouter(router *gin.Engine) {
@@ -30,7 +32,41 @@ func SetupRouter(router *gin.Engine) {
 		mq.GET("/put", rabbitmq.Put)
 		//mq.GET("/get", rabbitmq.Get)
 		mq.GET("/set", timer.Set)
-
 	}
+
+	flag := router.Group("flag")
+	{
+		flag.GET("/get", test.Get)
+		flag.GET("/get/:type", test.Get)
+	}
+
+	router.GET("/t", func(c *gin.Context) {
+
+		//message1 := make(chan int)
+		message2 := make(chan int)
+
+		go func() {
+			for i := 0; i < 10; i++ {
+				flag := <-message2
+				if flag > 5 {
+					log.Println("message2 已经 > 5")
+				}
+				//message1 <- i
+			}
+		}()
+
+		go func() {
+			for i := 0; i < 10; i++ {
+				message2 <- i
+			}
+		}()
+
+		for {
+			//msg1 := <- message1
+			msg2 := <-message2
+			//log.Printf("message1: %s \n", string(msg1))
+			fmt.Print(msg2)
+		}
+	})
 
 }
